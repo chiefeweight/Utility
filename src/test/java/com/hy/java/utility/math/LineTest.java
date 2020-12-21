5cd033c0e112f591c2875cdd097bd4d278a1a8f3
origin <https://gitee.com/chiefeweight/microservices.git>
2020-12-21 12:22:45 +0800

    下一个更新将是对math的包内类进行划分

--- src/test/java/com/hy/java/utility/math/LineTest.java
+++ src/test/java/com/hy/java/utility/math/LineTest.java
@@ -7,6 +7,6 @@ public class LineTest {
 	public void distanceTest() {
 		Point point = new Point(6.0, -4.0);
 		Segment segment = new Segment(new Point(1.0, -1.0 / 8.0), new Point(0.0, 5.0 / 8.0));
-		System.out.println("点(" + point.x + "," + point.y + ")到直线"+"y=" + segment.k.doubleValue() + "x+" + segment.b.doubleValue()+"的距离为" + Line.pointToLine(point, segment));
+		System.out.println("点(" + point.x + "," + point.y + ")到直线" + "y=" + segment.k.doubleValue() + "x+" + segment.b.doubleValue() + "的距离为" + Line.pointToLine(point, segment));
 	}
 }
